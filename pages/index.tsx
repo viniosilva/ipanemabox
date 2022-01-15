047a506416f324f99986e97058c44af62152caac
viniosilva <oliveira.vinicius.92@gmail.com>
2022-01-15 11:08:30 -0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default () => {
+  return (
+    <>
+      <Head>
+        <title>Ipanema Box</title>
+        <meta name="description" content="Ipanema Box" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <p>Hello world! (:</p>
+      </main>
+    </>
+  );
+};
